4a8ea1c5e178802017ef5e043297c8247931a66b
Claudine Chionh <cc@claudinec.net>
2015-04-22 17:37:31 +1000

    Sorted out paths. Need cleanup.

--- web/index.php
+++ web/index.php
@@ -22,8 +22,10 @@ $app['debug'] = true;
  */
 require __DIR__ . '/../config/nationbuilder.php';
 $appUrl         = 'http://energyfreedom-pledge.local:8888/';
-$redirectUrl    = $appUrl . 'oauth_callback';
-$authorizeUrl   = 'https://beyondzeroemissions.nationbuilder.com/oauth/authorize';
+$redirectUrl    = $appUrl . 'pledges';
+
+$baseApiUrl     = 'https://beyondzeroemissions.nationbuilder.com';
+$authorizeUrl   = $baseApiUrl . '/oauth/authorize';
 
 $client         = new OAuth2\Client($clientId, $clientSecret);
 $authUrl        = $client->getAuthenticationUrl($authorizeUrl, $redirectUrl);
@@ -45,40 +47,40 @@ $app->error(function (\Exception $e, $code) {
  * Default path – pledge lookup.
  */
 $app->get('/', function() use ($app, $client) {
-    $baseApiUrl = 'https://beyondzeroemissions.nationbuilder.com';
-    global $authUrl;
+    // global $authUrl, $token;
+    global $authUrl, $baseApiUrl;
 
+    // If we don't have an auth code, redirect to auth url.
+    // If we do, redirect to the pledge app.
     if (!isset($_GET['code'])) {
-      return $app->redirect($authUrl);
+        return $app->redirect($authUrl);
     }
     else {
-      $response = $client->fetch($baseApiUrl . '/api/v1/sites.json');
-      return $response;
+      return $app->redirect($redirectUrl);
     }
 
-    // Is user logged in?
-    // DEBUG HERE
-    // $response = $client->fetch($baseApiUrl . '/api/v1/people/me.json');
-    // $result = json_decode($response);
-    // $email = $response['result']['person']['email'];
-    // return new Response("Your email address is " . $email);
-    // print_r($response);
-    // return new Response("You are logged in as " . $response['result']['person']['full_name']);
-});
+    // Do something.
+            $response = $client->fetch($baseApiUrl . '/api/v1/people/me.json');
+    // See if we got a valid response back or an error.
+    if (isset($response['result']['error'])) {
+			$error = "<b>Unknown error:</b> " . $response['result']['error'] . " - "
+				. $response['result']['error_description'] . "<br>";
 
-/**
- * Authenticate to NationBuilder.
- */
-// $app->get('/auth', function () use ($app, $client) {
-//     global $authUrl;
-//     return $app->redirect($authUrl);
-// });
+	// End execution and display error message.
+	die($error);
+    }
+
+	    print_r($response);
+	    // 	    return $response;
+	    return "OK!";
+
+});
 
 /**
  * OAuth callback path.
  */
-$app->get('/oauth_callback', function () use ($app, $client) {
-    global $appUrl, $redirectUrl;
+$app->get('/pledges', function () use ($app, $client) {
+    global $appUrl, $baseApiUrl, $redirectUrl;
     $code = $app['request']->get('code');
 
     // Generate a token response.
@@ -86,16 +88,50 @@ $app->get('/oauth_callback', function () use ($app, $client) {
     $params = array('code' => $code, 'redirect_uri' => $redirectUrl);
     $response = $client->getAccessToken($accessTokenUrl, 'authorization_code', $params);
 
+    // See if we got a valid token back or an error.
+    if (isset($response['result']['error'])) {
+	switch($response['result']['error']) {
+		case 'invalid_grant':
+ 			$error = "<b>ERROR</b>: Invalid Grant. This code is invalid, expired, or revoked.<br>";
+			break;
+
+		default:
+			$error = "<b>Unknown error:</b> " . $response['result']['error'] . " - "
+				. $response['result']['error_description'] . "<br>";
+			break;
+	}
+
+	// End execution and display error message.
+	die($error);
+    }
+
     // Set the client token.
     $token = $response['result']['access_token'];
+    //     $client->setAccessTokenType(1);
     $client->setAccessToken($token);
 
-    // Return to app.
-    return $app->redirect($appUrl);
-});
+    // Is user logged in?
+    $response = $client->fetch($baseApiUrl . '/api/v1/people/me.json');
+    $result = json_decode($response);
 
-/**
- * Query custom field values and pre-fill form with them.
- */
+
+    // See if we got a valid response back or an error.
+    if (isset($response['result']['error'])) {
+			$error = "<b>Unknown error:</b> " . $response['result']['error'] . " - "
+				. $response['result']['error_description'] . "<br>";
+
+	// End execution and display error message.
+	die($error);
+    }
+
+    // $email = $response['result']['person']['email'];
+    // return new Response("Your email address is " . $email);
+    return new Response("You are logged in as " . $response['result']['person']['full_name'] . ".");
+
+    /**
+     * Query custom field values and pre-fill form with them.
+     */
+
+});
 
 $app->run();
